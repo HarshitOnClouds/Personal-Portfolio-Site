91f9b3f5fd5d47bcd84f300c0fcc7a474b6b381c
HarshitOnClouds <harshitonclouds@gmail.com>
2025-06-29 03:01:02 +0530

    favicon changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Content from './Content'
 export default function App(){
 
   return(
-      <div className="main bg-neutral-800 m-0 p-3 md:flex min-h-screen min-w-99vw">
+      <div className="main bg-neutral-800 m-0 p-3 md:flex min-h-screen min-w-full">
          <Intropart/>
          <Content/>
       </div>
